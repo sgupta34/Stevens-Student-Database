daa393a7b5e0fe18babc3360a0278a3ef7faa5a4
Sumit Gupta <sgupta34@steven.edu>
2019-11-13 17:29:38 -0500

    Made all the required changes to code

--- Lab/HW11_Test_Sumit_Gupta.py
+++ Lab/HW11_Test_Sumit_Gupta.py
@@ -12,53 +12,21 @@ class TestRepository(unittest.TestCase):
     """
     Class to test all the methods in HW09_Sumit_Gupta.py
     """
-    def test_directory(self):
-        """
-        Test if directory/files exist or not
-        """
-        file_check = Repository("Test")
-        self.assertEqual(file_check.student_path, 'Test\\students.txt')
-        self.assertEqual(file_check.instructor_path, 'Test\\instructors.txt')
-        self.assertEqual(file_check.grade_path, 'Test\\grades.txt')
-        self.assertEqual(file_check.major_path, 'Test\\majors.txt')
-        with self.assertRaises(FileNotFoundError):
-            file_check = Repository(".\\do_not_exist")
-        with self.assertRaises(FileNotFoundError):
-            for _ in file_reading_gen('Test\\do_not_exist.txt'):
-                pass
-
     def test_stevens_info(self):
         """
         Test if all the content of the file are loaded correctly or not
         """
-        stevens = Repository('Test')
-        students_info = {'10103': ['10103', 'Baldwin, C', 'SFEN', {'SSW 567': 'A', 'SSW 564':\
-             'A-', 'SSW 687': 'B', 'CS 501': 'B'}],
-                         '10115': ['10115', 'Wyatt, X', 'SFEN', {'SSW 567': 'A', 'SSW 564': 'B+'\
-                             , 'SSW 687': 'A', 'CS 545': 'A'}],
-                         '10172': ['10172', 'Forbes, I', 'SFEN', {'SSW 555': 'A', 'SSW 567': 'A-'}],
-                         '10175': ['10175', 'Erickson, D', 'SFEN', {'SSW 567': 'A', 'SSW 564': 'A',\
-                              'SSW 687': 'B-'}],
-                         '10183': ['10183', 'Chapman, O', 'SFEN', {'SSW 689': 'A'}],
-                         '11399': ['11399', 'Cordova, I', 'SYEN', {'SSW 540': 'B'}],
-                         '11461': ['11461', 'Wright, U', 'SYEN', {'SYS 800': 'A', 'SYS 750': 'A-',\
-                              'SYS 611': 'A'}],
-                         '11658': ['11658', 'Kelly, P', 'SYEN', {'SSW 540': 'F'}],
-                         '11714': ['11714', 'Morton, A', 'SYEN', {'SYS 611': 'A', 'SYS 645': 'C'}],
-                         '11788': ['11788', 'Fuller, E', 'SYEN', {'SSW 540': 'A'}]
+        stevens = Repository('Test',False)
+        students_info = {'10103': ['10103', 'Jobs, S', 'SFEN', {'SSW 810': 'A-', 'CS 501':'B'}],
+                         '10115': ['10115', 'Bezos, J', 'SFEN', {'SSW 810': 'A', 'CS 546': 'F'}],
+                         '10183': ['10183', 'Musk, E', 'SFEN', {'SSW 555': 'A', 'SSW 810': 'A'}],
+                         '11714': ['11714', 'Gates, B', 'CS', {'SSW 810': 'B-', 'CS 546': 'A','CS 570': 'A-'}]
                          }
-        instructors_info = {'98765': ['98765', 'Einstein, A', 'SFEN', {'SSW 567': 4, 'SSW 540': 3}],
-                            '98764': ['98764', 'Feynman, R', 'SFEN', {'SSW 564': 3, 'SSW 687': 3, \
-                                'CS 501': 1, 'CS 545': 1}],
-                            '98763': ['98763', 'Newton, I', 'SFEN', {'SSW 555': 1, 'SSW 689': 1}],
-                            '98762': ['98762', 'Hawking, S', 'SYEN', {}],
-                            '98761': ['98761', 'Edison, A', 'SYEN', {}],
-                            '98760': ['98760', 'Darwin, C', 'SYEN', {'SYS 800': 1, 'SYS 750': 1, \
-                                'SYS 611': 2, 'SYS 645': 1}]}
-        majors_info = {'SFEN': ['SFEN', ('SSW 555', 'SSW 564', 'SSW 567', 'SSW 540'), ('CS 513', \
-            'CS 545', 'CS 501')],
-                       'SYEN': ['SYEN', ('SYS 800', 'SYS 612', 'SYS 671'), ('SSW 565', 'SSW 810', \
-                           'SSW 540')]}
+        instructors_info = {'98764': ['98764', 'Cohen, R', 'SFEN', {'CS 546': 1}],
+                            '98763': ['98763', 'Rowland, J', 'SFEN', {'SSW 810': 4, 'SSW 555': 1}],
+                            '98762': ['98762', 'Hawking, S', 'CS', {'CS 501': 1, 'CS 546': 1, 'CS 570': 1}]}
+        majors_info = {'SFEN': ['SFEN', ('SSW 540', 'SSW 555', 'SSW 810'),('CS 501', 'CS 546')],
+                       'CS': ['CS', ('SYS 800', 'SYS 612', 'SYS 671'),('SSW 565', 'SSW 810')]}
         students_dic = dict()
         for cwid, student in stevens.students.items():
             students_dic[cwid] = student.get_whole_info()
@@ -78,31 +46,27 @@ class TestRepository(unittest.TestCase):
         """
         Test student successfully completed courses, remaining required courses and electives
         """
-        stevens = Repository('Test')
-        courses_info = {'10103': [['SSW 567', 'SSW 564', 'SSW 687', 'CS 501'], ['SSW 540', \
-            'SSW 555'], None],
-                        '10115': [['SSW 567', 'SSW 564', 'SSW 687', 'CS 545'], ['SSW 540', \
-                            'SSW 555'], None],
-                        '10172': [['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', \
-                            'CS 513', 'CS 545']],
+        stevens = Repository('Test',False)
+        courses_info = {'10103': [['CS 501', 'SSW 810'], {'SSW 555', 'SSW 540'}, None],
+                        '10115': [['SSW 810'], {'SSW 555', 'SSW 540'}, {'CS 501', 'CS 546'}],
+                        '10183': [['SSW 555', 'SSW 810'], {'SSW 540'}, {'CS 501', 'CS 546'}],
                         '10175': [['SSW 567', 'SSW 564', 'SSW 687'], ['SSW 540', 'SSW 555'], \
                             ['CS 501', 'CS 513', 'CS 545']],
-                        '10183': [['SSW 689'], ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], \
-                            ['CS 501', 'CS 513', 'CS 545']],
-                        '11399': [['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], None],
-                        '11461': [['SYS 800', 'SYS 750', 'SYS 611'], ['SYS 612', 'SYS 671'], \
-                            ['SSW 540', 'SSW 565', 'SSW 810']],
-                        '11658': [[], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', \
-                            'SSW 565', 'SSW 810']],
-                        '11714': [['SYS 611', 'SYS 645'], ['SYS 612', 'SYS 671', 'SYS 800'], \
-                            ['SSW 540', 'SSW 565', 'SSW 810']],
-                        '11788': [['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], None]}
+                        '11714': [['CS 546', 'CS 570', 'SSW 810'], None, None]}
         courses_dic = dict()
         for cwid, student in stevens.students.items():
             courses_dic[cwid] = stevens.majors[student.major].update_courses(student.courses)
         self.assertTrue(courses_dic, courses_info)
+    def test_db(self):
+        stevens = Repository('Test', False)
+        instructor_db_info = [(98762, 'Hawking, S', 'CS', 'CS 501', 1), 
+        (98762, 'Hawking, S', 'CS', 'CS 546', 1), (98762, 'Hawking, S', 'CS', 'CS 570', 1),
+        (98763, 'Rowland, J', 'SFEN', 'SSW 555', 1), (98763, 'Rowland, J', 'SFEN', 'SSW 810', 4),
+        (98764, 'Cohen, R', 'SFEN', 'CS 546', 1)]
+        self.assertEqual(stevens.query_list, instructor_db_info)
+
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
     # To execute main function that prints the pretty table
     print("\n===============================================================================")
-main()
+    main()
